17c4d5611835f29d23e53f8efd93f39ec9c3f4b8
Juan Melo Pérez <juanmlstm@gmail.com>
2022-05-30 23:10:33 +0100

    Added navbar to the project

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
+import { BrowserRouter as Router } from 'react-router-dom';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <>
+      <Router>
+        <Navbar />
+      </Router>
+    </>
   )
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'main': '#171717',
+      },
+    },
   },
   plugins: [],
 }
