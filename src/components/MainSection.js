516c88c1c72593893404775e47da729bae747dfd
Juan Melo Pérez <juanmlstm@gmail.com>
2022-06-02 12:49:33 +0100

    Added new compoenentand fixed some styles in navbar

---
+++ src/components/MainSection.js
@@ -0,0 +1,20 @@
+import React from "react";
+import '../App.css';
+import './MainSection.css';
+
+function MainSection() {
+    return (
+        <>
+            <div className="px-6 my-12">
+                <div className="relative max-w-2xl mx-auto p-8">
+                    <h1 className="text-white text-5xl font-semibold">Hello.</h1>
+                    <div className="mt-8">
+                        <p className="text-gray-300 text-lg">I am Juan Melo, a FullStack developer passionate about open source.</p>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default MainSection;
--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -1,9 +1,11 @@
 import React from "react";
 import '../../App.css';
+import MainSection from "../MainSection";
 
 function Resume() {
     return (
         <>
+            <MainSection />
         </>
     )
 }
