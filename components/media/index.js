291ff130a8363238346365f327e42b2bd1f18d92
Juan Melo Pérez <juanmlstm@gmail.com>
2022-12-04 14:17:31 +0000

    fix: not found image urls

--- components/media/index.js
+++ components/media/index.js
@@ -1,8 +1,12 @@
+import media1 from '../../public/assets/projects/crypto.png'
+import media2 from '../../public/assets/projects/quizz.png'
+import media3 from '../../public/assets/projects/raptor.png'
+
 export const projects = [
   {
     title: 'CryptoKeeper',
     slug: 'cryptokeeper',
-    banner: '/../public/assets/projects/crypto.png',
+    banner: media1,
     website: 'https://github.com/jmelop/CryptoKeeperFront',
     description:
       'Open source app that allow you to save your crypto data and check the market in real live. The app has multiple charts to check your monthly purchases or the value of your portfolio.',
@@ -10,7 +14,7 @@ export const projects = [
       'Open source app that allow to save your crypto data and check the market in real live.',
     repository: 'https://github.com/jmelop/CryptoKeeperFront',
     screenshots: [
-      '/../public/assets/projects/crypto.png'
+      media1
     ],
     deployment: {
       web: 'https://github.com/jmelop/CryptoKeeperFront',
@@ -20,7 +24,7 @@ export const projects = [
   {
     title: 'Quizz',
     slug: 'quizz',
-    banner: '/../public/assets/projects/quizz.png',
+    banner: media2,
     website: 'https://github.com/jmelop/Quizz',
     description:
       'Quizz will help you to learn new languages. Save your vocabulary and play to learn new words.',
@@ -28,7 +32,7 @@ export const projects = [
       'Quizz will help you to learn new languages. Save your vocabulary and play to learn new words.',
     repository: 'https://github.com/jmelop/Quizz',
     screenshots: [
-      '/../public/assets/projects/quizz.png'
+      media2
     ],
     deployment: {
       web: 'https://github.com/jmelop/Quizz',
@@ -38,7 +42,7 @@ export const projects = [
   {
     title: 'Raptor',
     slug: 'raptor',
-    banner: '/../public/assets/projects/raptor.png',
+    banner: media3,
     website: 'https://github.com/jmelop/Raptor',
     description:
       'Raptor is a ecommerce where you can buy the last drones of the market.',
@@ -46,7 +50,7 @@ export const projects = [
       'Raptor is a ecommerce where you can buy the last drones of the market.',
     repository: 'https://github.com/jmelop/Raptor',
     screenshots: [
-      '/../public/assets/projects/raptor.png'
+      media3
     ],
     deployment: {
       web: 'https://github.com/jmelop/Raptor',
