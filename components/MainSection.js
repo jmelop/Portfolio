45578f8b597c4dad2ac5d495ab3a3e0464f21c03
Juan Melo Pérez <juanmlstm@gmail.com>
2023-06-12 22:27:26 +0100

    feat: update legacy code

--- components/MainSection.js
+++ components/MainSection.js
@@ -2,59 +2,77 @@ import React from "react";
 import Link from 'next/link';
 
 function MainSection() {
-    return (
-        <>
-            <div className="px-6 my-16">
-                <div className="relative max-w-2xl mx-auto p-12">
-                    <h1 className="text-white text-5xl font-semibold">Hello.</h1>
-                    <div className="mt-8 prose">
-                        <p className="text-gray-300 text-lg">I am Juan Melo, a software developer passionate about open source and Full Stack Development
-                            trying to build new things. Focused on reading, learning, and building.</p>
-                        <div className="mt-4">
-                            <p className="text-gray-300 text-lg inline">Sometimes, I write </p>
-                            <Link href="/blog">
-                                <a target="_blank" rel="noreferrer"
-                                    className="text-white text-lg underline decoration-sky-500 hover:text-gray-300">blog posts</a>
-                            </Link>
-                            <p className="text-gray-300 text-lg inline"> and I have a </p>
-                            <Link href="/projects">
-                                <a target="_blank" rel="noreferrer"
-                                    className="text-white text-lg underline decoration-sky-500 hover:text-gray-300">todo list</a>
-                            </Link>
-                            <p className="text-gray-300 text-lg inline"> to keep track of my open source projects.</p>
-                        </div>
-                        <div className="mt-12 text-gray-300">
-                            <span>Find me on </span>
-                            <Link href="https://github.com/jmelop">
-                                <a target="_blank" rel="noreferrer"
-                                    className="text-white underline decoration-sky-500 hover:text-gray-300">Github,</a>
-                            </Link>
-                            <span>{' '}</span>
-                            <Link href="https://www.linkedin.com/in/juan-melo-p%C3%A9rez">
-                                <a target="_blank" rel="noreferrer"
-                                    className="text-white underline decoration-sky-500 hover:text-gray-300">Linkedin,</a>
-                            </Link>
-                            <span>{' '}</span>
-                            <Link href="https://www.instagram.com/jmelopdev/">
-                                <a target="_blank" rel="noreferrer"
-                                    className="text-white underline decoration-sky-500 hover:text-gray-300">Instagram,</a>
-                            </Link>
-                            <span>{' '}</span>
-                            <Link href="https://twitter.com/jmelop_">
-                                <a target="_blank" rel="noreferrer"
-                                    className="text-white underline decoration-sky-500 hover:text-gray-300">Twitter</a>
-                            </Link>
-                            <span>{' '}</span>
-                            <span>or mail me at </span>
-                            <Link href="mailto:jmelopdev@gmail.com">
-                                <a className="text-white hover:text-gray-300">jmelopdev@gmail.com</a>
-                            </Link>
-                        </div>
+    return <>
+        <div className="px-6 my-16">
+            <div className="relative max-w-2xl mx-auto p-12">
+                <h1 className="text-white text-5xl font-semibold">Hello.</h1>
+                <div className="mt-8 prose">
+                    <p className="text-gray-300 text-lg">I am Juan Melo, a software developer passionate about open source and Full Stack Development
+                        trying to build new things. Focused on reading, learning, and building.</p>
+                    <div className="mt-4">
+                        <p className="text-gray-300 text-lg inline">Sometimes, I write </p>
+                        <Link
+                            href="/blog"
+                            target="_blank"
+                            rel="noreferrer"
+                            className="text-white text-lg underline decoration-sky-500 hover:text-gray-300">
+                            blog posts
+                        </Link>
+                        <p className="text-gray-300 text-lg inline"> and I have a </p>
+                        <Link
+                            href="/projects"
+                            target="_blank"
+                            rel="noreferrer"
+                            className="text-white text-lg underline decoration-sky-500 hover:text-gray-300">
+                            todo list
+                        </Link>
+                        <p className="text-gray-300 text-lg inline"> to keep track of my open source projects.</p>
+                    </div>
+                    <div className="mt-12 text-gray-300">
+                        <span>Find me on </span>
+                        <Link
+                            href="https://github.com/jmelop"
+                            target="_blank"
+                            rel="noreferrer"
+                            className="text-white underline decoration-sky-500 hover:text-gray-300">
+                            Github,
+                        </Link>
+                        <span>{' '}</span>
+                        <Link
+                            href="https://www.linkedin.com/in/juan-melo-p%C3%A9rez"
+                            target="_blank"
+                            rel="noreferrer"
+                            className="text-white underline decoration-sky-500 hover:text-gray-300">
+                            Linkedin,
+                        </Link>
+                        <span>{' '}</span>
+                        <Link
+                            href="https://www.instagram.com/jmelopdev/"
+                            target="_blank"
+                            rel="noreferrer"
+                            className="text-white underline decoration-sky-500 hover:text-gray-300">
+                            Instagram,
+                        </Link>
+                        <span>{' '}</span>
+                        <Link
+                            href="https://twitter.com/jmelop_"
+                            target="_blank"
+                            rel="noreferrer"
+                            className="text-white underline decoration-sky-500 hover:text-gray-300">
+                            Twitter
+                        </Link>
+                        <span>{' '}</span>
+                        <span>or mail me at </span>
+                        <Link
+                            href="mailto:jmelopdev@gmail.com"
+                            className="text-white hover:text-gray-300">
+                            jmelopdev@gmail.com
+                        </Link>
                     </div>
                 </div>
             </div>
-        </>
-    )
+        </div>
+    </>;
 }
 
 export default MainSection;
--- components/Publication.js
+++ components/Publication.js
@@ -4,45 +4,45 @@ import Link from 'next/link';
 import raptorImage from '../public/dummies/images/banners_raptor.png';
 
 function Publication(props) {
-    return (
-        <>
-            <div className="relative w-full bg-white rounded-lg bg-main mb-2">
-                <div className="-mb-2">
-                    <Link href={props.href}>
-                        <Image width='525'
-                            height='355' className="rounded-t-lg object-cover w-full h-80 m-auto cursor-pointer"
-                            src={props.title !== 'Raptor' ? props.src : raptorImage} alt="blog" priority />
-                    </Link>
+    return <>
+        <div className="relative w-full rounded-lg bg-main mb-2">
+            <div className="-mb-2">
+                <Link href={props.href} legacyBehavior>
+                    <Image width='525'
+                        height='355' className="rounded-t-lg object-cover w-full h-80 m-auto cursor-pointer"
+                        src={props.title !== 'Raptor' ? props.src : raptorImage} alt="blog" priority />
+                </Link>
+            </div>
+            <div className="px-5 pb-5 border-x-2 border-b-2 border-gray-600 rounded-b-lg">
+                <Link href={props.href} legacyBehavior>
+                    <h5 className="text-xl pt-4 font-semibold tracking-tight text-white cursor-pointer">{props.title}</h5>
+                </Link>
+                <div className="flex items-center mt-2.5 mb-5">
+                    <p className="text-gray-200">{props.description}</p>
                 </div>
-                <div className="px-5 pb-5 border-x-2 border-b-2 border-gray-600 rounded-b-lg">
-                    <Link href={props.href}>
-                        <h5 className="text-xl pt-4 font-semibold tracking-tight text-white cursor-pointer">{props.title}</h5>
+                <div className="mt-2.5 flex">
+                    <Link
+                        href={props.href}
+                        className="text-white hover:text-gray-300 mr-1 font-semibold underline underline-offset-2 
+                        decoration-sky-500">
+                        Learn more
                     </Link>
-                    <div className="flex items-center mt-2.5 mb-5">
-                        <p className="text-gray-200">{props.description}</p>
-                    </div>
-                    <div className="mt-2.5 flex">
-                        <Link href={props.href}>
-                            <a className="text-white hover:text-gray-300 mr-1 font-semibold underline underline-offset-2 
-                            decoration-sky-500">Learn more</a>
-                        </Link>
-                        <svg
-                            width="24"
-                            height="24"
-                            viewBox="0 0 24 24"
-                            fill="none"
-                            xmlns="http://www.w3.org/2000/svg"
-                            className="text-white hover:text-gray-300 mt-0.5 cursor-pointer">
-                            <path
-                                d="M15.0378 6.34317L13.6269 7.76069L16.8972 11.0157L3.29211 11.0293L3.29413 13.0293L16.8619 13.0157L13.6467 
-                                16.2459L15.0643 17.6568L20.7079 11.9868L15.0378 6.34317Z"
-                                fill="currentColor" />
-                        </svg>
-                    </div>
+                    <svg
+                        width="24"
+                        height="24"
+                        viewBox="0 0 24 24"
+                        fill="none"
+                        xmlns="http://www.w3.org/2000/svg"
+                        className="text-white hover:text-gray-300 mt-0.5 cursor-pointer">
+                        <path
+                            d="M15.0378 6.34317L13.6269 7.76069L16.8972 11.0157L3.29211 11.0293L3.29413 13.0293L16.8619 13.0157L13.6467 
+                            16.2459L15.0643 17.6568L20.7079 11.9868L15.0378 6.34317Z"
+                            fill="currentColor" />
+                    </svg>
                 </div>
             </div>
-        </>
-    )
+        </div>
+    </>;
 }
 
 export default Publication;
