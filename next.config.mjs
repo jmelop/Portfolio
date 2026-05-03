5843eb4cd2098b6c8143ad718dd5ffd091aa7d88
jmelop <jmelopdev@gmail.com>
2026-05-04 00:26:51 +0100

    chore: remove unused deps, simplify next config and add gtag utility

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,4 @@
-import withPlaiceholder from '@plaiceholder/next'
+/** @type {import('next').NextConfig} */
+const nextConfig = {};
 
-const nextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "firebasestorage.googleapis.com",
-        port: "",
-        pathname: "/v0/b/portfolio-4bb89.appspot.com/o/**",
-      },
-    ],
-  },
-};
-
-export default withPlaiceholder(nextConfig);
+export default nextConfig;
